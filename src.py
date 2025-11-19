1945fef9df0747ec927f9e55eb05da6910172418
tiffanymdz <quirostiffany721@gmail.com>
2025-11-19 11:10:28 -0600

    Se agregó código nuevo
    feat: se agrega lo del Modelado_POO más lo outliers del Procesador_EDA

--- src.py
+++ src.py
@@ -1,5 +1,6 @@
 from CargaDatos import CargadorDatos
 from Procesador_EDA import ProcesadorEDA
+from Modelado_POO import Jugador, Equipo
 
 # 1️⃣ Cargar dataset
 cargador = CargadorDatos(r"C:\Users\tiffa\OneDrive\Desktop\Big Data\Programación II\premier.csv")
@@ -26,3 +27,41 @@ print("-" * 60)
 
 df_limpio.to_csv(r"C:\Users\tiffa\OneDrive\Desktop\Big Data\Programación II\premier_limpio.csv", index=False)
 print("✅ Dataset limpio guardado como 'premier_limpio.csv'")
+
+# 3️⃣ Modelado POO de los jugadores y equipos
+
+jugadores = []
+for _, row in df_limpio.iterrows():
+    jugador = Jugador(
+        name=row["Player"],
+        team=row["Team"],
+        position=row["Position"],
+        minutes=row["Minutes"],
+        goals=row["Goals"],
+        assists=row["Assists"]
+    )
+    jugadores.append(jugador)
+
+equipo_mu = Equipo("Manchester United")
+for j in jugadores:
+    if j.team == "Manchester United":
+        equipo_mu.agregar_jugador(j)
+
+print("📌 Total goles MU:", equipo_mu.total_goles())
+print("📌 Total asistencias MU:", equipo_mu.total_assists())
+
+equipo_ful = Equipo("Fulham")
+for j in jugadores:
+    if j.team == "Fulham":
+        equipo_ful.agregar_jugador(j)
+
+print("📌 Total goles FUL :", equipo_ful.total_goles())
+print("📌 Total asistencias FUL:", equipo_ful.total_assists())
+
+equipo_liv = Equipo("Liverpool")
+for j in jugadores:
+    if j.team == "Liverpool":
+        equipo_liv.agregar_jugador(j)
+
+print("📌 Total goles LIV:", equipo_liv.total_goles())
+print("📌 Total asistencias LIV:", equipo_liv.total_assists())
